97bf8ffe976911c67784c105f9d35c630d3039b7
JionkJionk <jion.kw@gmail.com>
2025-05-21 13:48:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ document.getElementById("quiz-form").addEventListener("submit", function (e) {
     HP: 0, RW: 0, HG: 0, MM: 0, AD: 0, RH: 0, SS: 0, DM: 0, NL: 0
   };
 
-  // Question 1 scoring
+  // Question 1
   const q1 = form.get("q1");
   if (q1 === "strongly-agree") scores["HG"] += 2;
   else if (q1 === "agree") scores["MM"] += 1;
   else if (q1 === "disagree") scores["RW"] += 1;
   else if (q1 === "strongly-disagree") scores["DM"] += 2;
 
-  // Question 2 scoring
+  // Question 2
   const q2 = form.get("q2");
   if (q2 === "brave") scores["HP"] += 2;
   if (q2 === "clever") scores["HG"] += 2;
   if (q2 === "loyal") scores["RH"] += 2;
   if (q2 === "ambitious") scores["DM"] += 2;
 
-  // Find top character
+  // Top character
   const topChar = Object.keys(scores).reduce((a, b) => scores[a] > scores[b] ? a : b);
 
   const characterNames = {
@@ -36,5 +36,5 @@ document.getElementById("quiz-form").addEventListener("submit", function (e) {
   };
 
   const resultBox = document.getElementById("result");
-  resultBox.innerHTML = `<strong>You are most like:</strong> <br><span style="font-size:1.5em">${characterNames[topChar]}</span>`;
+  resultBox.innerHTML = `<strong>You are most like:</strong><br><span style="font-size:1.5em">${characterNames[topChar]}</span>`;
 });
